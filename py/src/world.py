d460557128753abc8b02597a4e34308834bfcfc5
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-04-11 19:15:35 +0000

    Flesh out and document World class

--- py/src/world.py
+++ py/src/world.py
@@ -1,10 +1,63 @@
 class World:
+    """
+    This class represents a world of cells.
+    It uses a 2D array of 1's and 0's to represent the cells.
+    """
 
-    def __init__(self):
+    def __init__(self, size: int) -> None:
+        """
+        Creates a new world of the specified size.
+        This will be a square world with sidelength size.
+
+        :param size: the size of the world
+        """
         pass
 
-    def is_alive(self, row: int, col: int):
+    @property
+    def size(self) -> int:
+        """
+        Retruns the size of the world.
+        """
+        return 1  # TODO - Replace with actual size
+
+    def __getitem__(self, key: tuple[int, int]) -> int:
+        """
+        Allows accessing the cell state at the specified (row, column) using array indexing.
+        Cells outside of bounds will be wrapped to the other side.
+
+        :param key: a tuple containing the row and column indices
+        :return: the state of the cell at the specified row and column
+        """
+        # row, col = key
+        # row = row % self.size
+        # col = col % self.size
+        # return self.cells[row][col]
+        return 0  # TODO - Replace with actual cell state
+
+    def __setitem__(self, key: tuple[int, int], value: int) -> None:
+        """
+        Allows setting the cell state at the specified (row, column) using array indexing.
+        Cells outside of bounds will be wrapped to the other side.
+        """
         pass
+
+    def is_alive(self, row: int, col: int) -> bool:
+        """
+        Determines if the cell at the specified row and column is alive.
+        Cells on borders wrap around to the other side when checking their neighbors.
+ 
+        :return: whether the cell at the specified location is alive
+        """
+        return True  # TODO - Replace with actual value
+
+    def count_neighbors(self, row: int, col: int) -> int:
+        """
+        Counts the number of alive neighbors of the cell at the specified row and column.
+        Cells on borders wrap around to the other side when counting their neighbors.
+
+        :return: the number of alive neighbors of the cell at the specified location
+        """
+        return 0  # TODO - Replace with actual value
 
 
 class Random(World):
