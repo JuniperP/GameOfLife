cdd209265ab936709f5b1b938e13bafe24758ff4
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-04-11 18:01:59 +0000

    Create basic skeleton of simulation and world

---
+++ py/src/simulation.py
@@ -0,0 +1,11 @@
+from world import World
+
+
+class Simulation:
+
+    def __init__(self, world: World):
+        self.world: World = world
+        pass
+
+    def run(self):
+        pass
---
+++ py/src/world.py
@@ -0,0 +1,25 @@
+class World:
+
+    def __init__(self):
+        pass
+
+    def is_alive(self, row: int, col: int):
+        pass
+
+
+class Random(World):
+
+    def __init__(self):
+        pass
+
+
+class Pulsar(World):
+
+    def __init__(self):
+        pass
+
+
+class Glider(World):
+
+    def __init__(self):
+        pass
