fd7174e42774b095b41784873d9c95896203387a
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-04-11 18:38:33 +0000

    Flesh out and document simulation skeleton

--- py/src/simulation.py
+++ py/src/simulation.py
@@ -1,11 +1,43 @@
 from world import World
+from enum import Enum
+
+
+class WorldType(Enum):
+    RANDOM = 0
+    PULSAR = 1
+    GLIDER = 2
 
 
 class Simulation:
+    """
+    This class represents a simulation of the Conway's Game of Life.
+    It provides the ability to step through a generation (iteration)
+    of the simulation.
+    """
 
-    def __init__(self, world: World):
-        self.world: World = world
+    def __init__(self, world_type: WorldType) -> None:
+        """
+        Creates a new simulation of the world type specified.
+
+        @param world_type: the type of world to use for the simulation
+        """
         pass
 
-    def run(self):
+    @property
+    def world(self) -> World:
+        """
+        Returns the current world.
+        """
+        return World()  # TODO - Replace with actual world
+
+    def step(self) -> None:
+        """
+        Steps the simulation forward one generation.
+        """
+        pass
+
+    def reset(self) -> None:
+        """
+        Resets the simulation to the initial state.
+        """
         pass
