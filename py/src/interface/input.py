d10117085763ff4351cc6f2d1ce7cc9c86aa78f1
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-04-13 20:33:37 +0000

    Add bare and undocumented skeleton of interface

---
+++ py/src/interface/input.py
@@ -0,0 +1,22 @@
+class InputManager:
+
+    def __init__(self):
+        pass
+
+
+class Slider:
+
+    def __init__(self):
+        pass
+
+
+class Dropdown:
+
+    def __init__(self):
+        pass
+
+
+class Button:
+
+    def __init__(self):
+        pass
