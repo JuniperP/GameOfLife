b519b690e1b9d62e5d6bc782b129337971803aa1
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-04-15 13:02:25 +0000

    Improve World class documentation

--- py/src/simul/world.py
+++ py/src/simul/world.py
@@ -11,54 +11,68 @@ class World:
 
         :param size: the size of the world
         """
-
-        pass
+        self._size = size
+        # self._array = np.zeros((size, size), dtype=np.int8)
 
     @property
     def size(self) -> int:
         """
         Returns the size of the world.
-        """
-        return 6  # TODO - Replace with actual size
 
-    def __getitem__(self, key: tuple[int, int]) -> int:
+        :return: the size of the world
+        :rtype: int
+        """
+        return self._size
+
+    def __getitem__(self, key: tuple[int, int]) -> bool:
         """
         Allows accessing the cell state at the specified (row, column) using array indexing.
         Cells outside of bounds will be wrapped to the other side.
 
-        :param key: a tuple containing the row and column indices
+        :param tuple[int, int] key: a tuple containing the row and column indices
         :return: the state of the cell at the specified row and column
+        :rtype: bool
         """
         # row, col = key
         # row = row % self.size
         # col = col % self.size
         # return self.cells[row][col]
 
-        return 0  # TODO - Replace with actual cell state
+        return False  # TODO - Replace with actual cell state
 
-    def __setitem__(self, key: tuple[int, int], value: int) -> None:
+    def __setitem__(self, key: tuple[int, int], value: bool) -> None:
         """
         Allows setting the cell state at the specified (row, column) using array indexing.
         Cells outside of bounds will be wrapped to the other side.
+
+        :param tuple[int, int] key: a tuple containing the row and column indices
+        :param bool value: the new state of the cell at the specified row and column
         """
         pass
 
-    def is_alive(self, row: int, col: int) -> bool:
+    def _is_alive(self, row: int, col: int) -> bool:
         """
-        Determines if the cell at the specified row and column is alive.
+        Determines if the cell at the specified row and column SHOULD be alive.
+        For example, an alive cell surrounded by 9 dead cells will die.
         Cells on borders wrap around to the other side when checking their neighbors.
- 
+
+        :param int row: the row index of the cell
+        :param int col: the column index of the cell
         :return: whether the cell at the specified location is alive
+        :rtype: bool
         """
 
         return True  # TODO - Replace with actual value
 
-    def count_neighbors(self, row: int, col: int) -> int:
+    def _count_neighbors(self, row: int, col: int) -> int:
         """
         Counts the number of alive neighbors of the cell at the specified row and column.
         Cells on borders wrap around to the other side when counting their neighbors.
 
+        :param int row: the row index of the cell
+        :param int col: the column index of the cell
         :return: the number of alive neighbors of the cell at the specified location
+        :rtype: int
         """
         return 0  # TODO - Replace with actual value
 
