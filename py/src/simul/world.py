c90a179d8404d4ade83e398b31838901d61cee81
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-04-15 01:12:05 +0000

    Flesh out and document 3 world types

--- py/src/simul/world.py
+++ py/src/simul/world.py
@@ -11,7 +11,7 @@ class World:
 
         :param size: the size of the world
         """
-        
+
         pass
 
     @property
@@ -34,7 +34,6 @@ class World:
         # col = col % self.size
         # return self.cells[row][col]
 
-        
         return 0  # TODO - Replace with actual cell state
 
     def __setitem__(self, key: tuple[int, int], value: int) -> None:
@@ -45,13 +44,13 @@ class World:
         pass
 
     def is_alive(self, row: int, col: int) -> bool:
-    
         """
         Determines if the cell at the specified row and column is alive.
         Cells on borders wrap around to the other side when checking their neighbors.
  
         :return: whether the cell at the specified location is alive
         """
+
         return True  # TODO - Replace with actual value
 
     def count_neighbors(self, row: int, col: int) -> int:
@@ -63,22 +62,50 @@ class World:
         """
         return 0  # TODO - Replace with actual value
 
-    
-
 
 class Random(World):
+    """
+    This class represents a random world. 
+    Each cell is either alive or dead based on the life probability value. 
+    """
 
-    def __init__(self):
+    def __init__(self, probability: float) -> None:
+        """
+        Creates a new random world.
+
+        :param float probability: the probability of a cell being alive
+        """
+        pass
+
+    def randomize(self) -> None:
+        """
+        Randomizes the world by setting each cell to either alive or dead based
+        on the probability value.
+        """
         pass
 
 
 class Pulsar(World):
+    """
+    This class represents a pulsar world.
+    Grid starts with cyclical pattern alternates between three different patterns   
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Creates a new pulsar world.
+        """
         pass
 
 
 class Glider(World):
+    """
+    This class represents a glider world.
+    The glider moves across the grid by moving the cells.
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Creates a new glider world.
+        """
         pass
